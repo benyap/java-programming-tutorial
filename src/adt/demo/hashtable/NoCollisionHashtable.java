2e466ae33714bfe2355d9a0ff1ab15580186a591
Benjamin Yap <bwyap@outlook.com>
2017-04-17 11:30:39 +0800

    Reorganised code

--- src/adt/demo/hashtable/NoCollisionHashtable.java
+++ src/adt/demo/hashtable/NoCollisionHashtable.java
@@ -2,6 +2,16 @@ package adt.demo.hashtable;
 
 public class NoCollisionHashtable<K, V> extends GenericHashtable<K, V>{
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public V get(K key) {
+		int pos = hash(key, table.length);
+		if (keys[pos] == null) {
+			throw new RuntimeException("No value exists for key '" + key + "'");
+		}
+		return (V) table[pos];
+	}
+	
 	@Override
 	public void remove(K key) {
 		int pos = hash(key, table.length);
@@ -14,16 +24,6 @@ public class NoCollisionHashtable<K, V> extends GenericHashtable<K, V>{
 	@Override
 	protected void collision(K key, int pos, V value) {
 		throw new RuntimeException("Collision occurred for keys '" + key + "' and '" + keys[pos] + "'");
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public V get(K key) {
-		int pos = hash(key, table.length);
-		if (keys[pos] == null) {
-			throw new RuntimeException("No value exists for key '" + key + "'");
-		}
-		return (V) table[pos];
 	}
 
 }
